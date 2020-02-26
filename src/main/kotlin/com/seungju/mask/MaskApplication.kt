588eedcf5e801c7d1ac7adfe13370b61a8dfc674
AlbusStar <poketred13@gmail.com>
2020-02-26 13:16:04 +0900

    초기 프로젝트 설정

---
+++ src/main/kotlin/com/seungju/mask/MaskApplication.kt
@@ -0,0 +1,11 @@
+package com.seungju.mask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<MaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/seungju/mask/MaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.seungju.mask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
